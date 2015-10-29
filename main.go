bd785fce3040c80f902f144d635de745d8fb52a6
Andrew Stuart <andrew.stuart2@gmail.com>
2015-10-28 20:39:59 -0700

    Add homedir dependency for cross-compilation

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/mitchellh/go-homedir"
 	"github.com/toqueteos/webbrowser"
 )
-import "os/user"
 
 const linksFileName = "links.json"
 
@@ -23,17 +23,16 @@ usage:
  - %s [open] <number> - Open the link at number <number>
  `, os.Args[0], os.Args[0], os.Args[0], os.Args[0], os.Args[0])
 
-var (
-	rlHome     = "~/.local/readinglinks/"
-	rlFileName = rlHome + linksFileName
-)
+var rlHome, rlFileName string
 
 func init() {
-	u, err := user.Current()
-	if err == nil {
-		rlHome = fmt.Sprintf("%s/.local/readinglinks/", u.HomeDir)
-		rlFileName = rlHome + linksFileName
+	dir, err := homedir.Dir()
+	if err != nil {
+		log.Fatal("Could not determine home directory for list storage.")
 	}
+
+	rlHome = fmt.Sprintf("%s/.local/readinglinks/", dir)
+	rlFileName = rlHome + linksFileName
 
 	if _, err := os.Stat(rlHome); err != nil {
 		err := os.MkdirAll(rlHome, 0770)
